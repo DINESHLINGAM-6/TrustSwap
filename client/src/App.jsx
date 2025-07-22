f2354569c1557929ffb77119dc86d6b19683521f
DRAGONDINESH <lingamdinesh6@gmail.com>
2025-07-22 23:46:24 +0530

    Initialised BE

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,16 @@
-
+import { Navbar,Welcome, Loader, Footer, Services, Transactions } from './components';
 const App = () => {
   return (
-    <div >
-      <h1 className="text-3xl font-bold underline ">
-        Hello world!
-      </h1>
+    <div className="min-h-screen">
+      <div className='gradient-bg-welcome'>
+        <Navbar />
+        <Welcome />
+      </div>
+      <Services />
+      <Transactions />
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
